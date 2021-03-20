1a01cec36e2501df9b8ca7c0bb3dac1b1096e2d4
mingaha <56760594+mingaha@users.noreply.github.com>
2021-03-20 17:24:09 +0100

    one

---
+++ angular/backend/Api/database.php
@@ -0,0 +1,24 @@
+<?php
+header("Access-Control-Allow-Origin: *"); //pour authorise php a ce execte sur tt serve 
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");// pour active tt les methode 
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mydb');
+
+function connect()
+{
+  $connect = mysqli_connect(DB_HOST ,DB_USER ,DB_PASS ,DB_NAME);
+
+  if (mysqli_connect_errno($connect)) {
+    die("Failed to connect:" . mysqli_connect_error());
+  }
+
+  mysqli_set_charset($connect, "utf8");
+
+  return $connect;
+}
+
+$con = connect();
