acef14a5e7ba293a26193ae792eb2a058015b07d
mingaha <56760594+mingaha@users.noreply.github.com>
2021-03-20 18:40:09 +0100

    1

--- angular/frontend1/src/app/app.module.ts
+++ angular/frontend1/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from  '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ angular/frontend1/src/app/policy.ts
@@ -0,0 +1,7 @@
+
+
+export  class  Policy {
+  id: number=0;
+  number:  number=0;
+  amount:  number=0;
+}
